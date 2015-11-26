65e966ddf7bcaa907985551a9fd63b0fb5f2e09a
Hana Lee <i@leehana.co.kr>
2015-11-27 02:52:05 +0900

    네이버 지도 최소 동작 코드 추가.

--- blind-navigator-ios/ViewController.swift
+++ blind-navigator-ios/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//         Do any additional setup after loading the view, typically from a nib.
+
+		let nmap = NMapView.init(frame: self.view.frame)
+		let apiKey = "c046b7b09869549d4f8201840ebce0b1"
+		nmap.setApiKey(apiKey)
+		
+//		[_mapView setMapCenter:NGeoPointMake(126.978371, 37.5666091) atLevel:11];
+
+		nmap.setPanEnabled(true)
+		nmap.setMapCenter(NGeoPoint(longitude: 126.978371, latitude: 37.5666091), atLevel: 15)
+//		nmap.setMapCenter(NGeoPoint(longitude: 126.978371, latitude: 37.5666091), atLevel: 11)
+
+		self.view.addSubview(nmap)
+		
     }
+	
+//	- (void) onMapView:(NMapView *)mapView initHandler:(NMapError *)error;
+	
+	
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ blind-navigator-ios/nmapBridge.h
@@ -0,0 +1,9 @@
+//
+//  nmapBridge.h
+//  blind-navigator-ios
+//
+//  Created by LeeHana on 2015. 11. 26..
+//  Copyright © 2015년 LeeHana. All rights reserved.
+//
+
+#import "NMapView.h"
